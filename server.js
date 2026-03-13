5723c7836c1b14e8a67dd88f935d8b1dfc396d5e
haraldur <halliottosson@gmail.com>
2026-03-13 10:20:55 +0000

    completed server js header and footer, and movied port to an environment variable

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
 
-const startrekReouter = require('./src/routes/startrek.routes');
-const { title } = require(process);
+const startrekRouter = require('./src/routes/startrek.routes');
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.set('viwes', path,path.join(__dirname, 'src/'views));
+app.set('viwes', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', startrekReouter);
+app.use('/', startrekRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
